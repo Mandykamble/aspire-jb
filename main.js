473c2a0a2e7590862d78381fe63a25d347699497
ankurdg107 <ankurdattatrayg@gmail.com>
2023-02-24 23:03:48 +0530

    Changed code5

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 // To localhost
-mongoose.connect('mongodb+srv://Nidhish:gofVgBxqPi9JOIqm@cluster0.nuz1fzr.mongodb.net/mentorship?retryWrites=true&w=majority', { useNewUrlParser: true,
+mongoose.connect('mongodb+srv://Nidhish:gofVgBxqPi9JOIqm@cluster0.nuz1fzr.mongodb.net/mentorship?retryWrites=true&w=majority', 
+{ useNewUrlParser: true,
 useUnifiedTopology: true,
 useCreateIndex: true,
 useFindAndModify: true, });
